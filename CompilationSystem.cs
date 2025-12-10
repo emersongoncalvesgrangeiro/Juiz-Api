e55d70c9f0bef8840b3272ca5e0e86f817faf8ef
yonn_drakkonuer <emersongoncalves.grangeiro@gmail.com>
2025-12-09 21:37:49 -0300

    Compilação completa

--- CompilationSystem.cs
+++ CompilationSystem.cs
@@ -3,15 +3,8 @@ using System.Diagnostics;
 namespace CompilationSystem {
   public class CompilationSystem_ {
     public string output = "", error = "";
-    private List<string> list = new List<string>();
     public CompilationSystem_() {
 
-    }
-    public void Add_(List<string> a) {
-      foreach (string b in a) {
-        list.Add(b);
-        Console.WriteLine(b);
-      }
     }
     public async Task Checker(string path) {
       string[] archives = Directory.GetFiles(path).Where(v => v.EndsWith(".c") || v.EndsWith(".h")).ToArray();
@@ -33,7 +26,6 @@ namespace CompilationSystem {
     }
     public async Task Compiler(string path, string type) {
       var process = new ProcessStartInfo();
-      string archivelist = "";
       process.FileName = "/bin/bash";
       process.RedirectStandardOutput = true;
       process.RedirectStandardError = true;
@@ -43,7 +35,6 @@ namespace CompilationSystem {
         if (type == "java") {
           process.Arguments = $"-c \"cd '{path}'/ && javac *.java -d out";
         } else if (type == "c") {
-          archivelist = string.Join(" ", list.Select(x => $"'{x}'"));
           process.Arguments = $"-c \"cd '{path}'/ && gcc *.c *.h -o out";
         }
         using var processstart = Process.Start(process);
